9477c1bbfc24914323533121207164b07c703ba5
4002-Nonye <royaltysungbin@gmail.com>
2024-10-16 11:40:22 +0100

    media query

--- src/features/invoice/CreateEditInvoice.jsx
+++ src/features/invoice/CreateEditInvoice.jsx
@@ -131,7 +131,7 @@ function CreateEditInvoice({ setShowForm }) {
     return (
         <form
             onSubmit={handleSubmit(onSubmit, onError)}
-            className="fixed bottom-0 left-0 top-0 z-40 h-full w-2/4 rounded-lg bg-white-200 px-5 pt-32 shadow-lg lg:pl-36 lg:pr-10 lg:pt-16 dark:bg-darkblue-500"
+            className="fixed bottom-0 left-0 top-0 z-40 h-full w-full md:w-3/4 lg:w-2/4 rounded-lg bg-white-200 px-5 pt-32 shadow-lg lg:pl-36 lg:pr-10 lg:pt-16 dark:bg-darkblue-500"
             action=""
         >
             <div className="flex h-full flex-col space-y-4 overflow-scroll pb-40">
